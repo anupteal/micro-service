9b294bc085ca18b3170628f6dbf57cefe3ad300d
anupteal <anup.singh@igtsolutions.com>
2020-11-24 20:36:18 +0530

    updated

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd employee-service
-docker build -t registry.cn-hangzhou.aliyuncs.com/shawn_repo/employee:1.1 .  && cd ../
-cd department-service
-docker build -t registry.cn-hangzhou.aliyuncs.com/shawn_repo/department:1.1 . && cd ../
-cd organization-service
-docker build -t registry.cn-hangzhou.aliyuncs.com/shawn_repo/organization:1.1 .
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f kubernetes/projects/employee-deployment.yaml
-kubectl apply -f kubernetes/projects/department-deployment.yaml
-kubectl apply -f kubernetes/projects/organization-deployment.yaml
-kubectl apply -f kubernetes/ingress.yaml
